dc2695ef91094775ef1d3761f14772b23c9b9eaa
Bruno Salvatore <143473123+Brunosalvatore2604@users.noreply.github.com>
2025-07-23 15:53:23 +0000

    negro

--- api/server.js
+++ api/server.js
@@ -196,7 +196,7 @@ app.post('/api/turnos', async (req, res) => {
         const agendaHtml = `
             <h2>Agenda de reservas pendientes</h2>
             <table border="1" cellpadding="8" cellspacing="0" style="border-collapse:collapse;font-family:sans-serif;font-size:1em;">
-                <thead style="background:#BBA3D0;color:#fff;">
+                <thead style="background:#BBA3D0;color:black;">
                     <tr>
                         <th>Fecha</th><th>Hora</th><th>Profesional</th><th>Servicio</th><th>Cliente (correo)</th><th>Teléfono</th>
                     </tr>
@@ -285,7 +285,7 @@ app.post('/api/cancelar/:token', async (req, res) => {
         const agendaHtml = `
             <h2>Agenda de reservas pendientes</h2>
             <table border=\"1\" cellpadding=\"8\" cellspacing=\"0\" style=\"border-collapse:collapse;font-family:sans-serif;font-size:1em;\">
-                <thead style=\"background:#BBA3D0;color:#fff;\">
+                <thead style=\"background:#BBA3D0;color:#black;\">
                     <tr>
                         <th>Fecha</th><th>Hora</th><th>Profesional</th><th>Servicio</th><th>Cliente (correo)</th><th>Teléfono</th>
                     </tr>
@@ -418,7 +418,7 @@ app.delete('/api/admin/reservas/:id', async (req, res) => {
         const agendaHtml = `
             <h2>Agenda de reservas pendientes</h2>
             <table border="1" cellpadding="8" cellspacing="0" style="border-collapse:collapse;font-family:sans-serif;font-size:1em;">
-                <thead style="background:#BBA3D0;color:#fff;">
+                <thead style="background:#BBA3D0;color:black;">
                     <tr>
                         <th>Fecha</th><th>Hora</th><th>Profesional</th><th>Servicio</th><th>Cliente (correo)</th><th>Teléfono</th>
                     </tr>
@@ -510,7 +510,7 @@ app.put('/api/admin/reservas/:id', async (req, res) => {
         const agendaHtml = `
             <h2>Agenda de reservas pendientes</h2>
             <table border="1" cellpadding="8" cellspacing="0" style="border-collapse:collapse;font-family:sans-serif;font-size:1em;">
-                <thead style="background:#BBA3D0;color:#fff;">
+                <thead style="background:#BBA3D0;color:black;">
                     <tr>
                         <th>Fecha</th><th>Hora</th><th>Profesional</th><th>Servicio</th><th>Cliente (correo)</th><th>Teléfono</th>
                     </tr>
